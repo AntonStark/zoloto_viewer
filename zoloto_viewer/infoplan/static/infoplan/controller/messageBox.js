92dc378b3a0cc92835965b50daa87d3d785909ad
anton iakovev <lantar23@yandex.ru>
2020-04-13 15:22:04 +0300

    refactor message placement method

--- zoloto_viewer/infoplan/static/infoplan/controller/messageBox.js
+++ zoloto_viewer/infoplan/static/infoplan/controller/messageBox.js
@@ -1,6 +1,6 @@
 "use strict";
 function ControllerMessageBox(render) {
-    let renderedMessagesIndex = {};     // marker_uid -> MessageBoxNode
+    let _renderedMessagesIndex = {};     // marker_uid -> {messContainer: MessageBoxNode}
     let visibleMessagesIndex  = {};     // marker_uid -> visibility (bool)
     let markerElementIndex    = {};     // marker_uid -> MarkerElement
     const renderMessage = render;
@@ -8,24 +8,35 @@ function ControllerMessageBox(render) {
     function _registerMessageItem(marker_uid, messageObj) {
         const messNode = messageObj.messContainer;
 
-        if (renderedMessagesIndex[marker_uid] !== undefined) {
-            const storedContainer = renderedMessagesIndex[marker_uid].messContainer;
+        if (_renderedMessagesIndex[marker_uid] !== undefined) {
+            const storedContainer = _renderedMessagesIndex[marker_uid].messContainer;
             if (storedContainer !== messNode)
-                renderedMessagesIndex[marker_uid].remove();
+                _renderedMessagesIndex[marker_uid].remove();
         }
-        renderedMessagesIndex[marker_uid] = {messContainer: messNode, messLink: undefined};
+        _renderedMessagesIndex[marker_uid] = {messContainer: messNode, messLink: undefined};
     }
     function renderedMessagesIds() {
-        return Object.keys(renderedMessagesIndex);
+        return Object.keys(_renderedMessagesIndex);
     }
     function getContainerOrNull(marker_uid) {
-        if (renderedMessagesIndex[marker_uid] !== undefined)
-            return renderedMessagesIndex[marker_uid].messContainer;
+        if (_renderedMessagesIndex[marker_uid] !== undefined)
+            return _renderedMessagesIndex[marker_uid].messContainer;
         else
             return null;
     }
 
+    function deduceContainer(layerTitle) {
+        const c = document.getElementsByClassName('layer_messages layer-' + layerTitle);
+        if (c.length > 0)
+            return c[0];
+        else
+            return undefined;
+    }
     function acquireMessagePosition(marker_uid) {
+        for (const markerUid of renderedMessagesIds()) {
+            const box = getContainerOrNull(markerUid);
+            console.debug(box.getBoundingClientRect());
+        }
         const [rawMarX, rawMarY] = markerCirclesManager.position(marker_uid);
         const planScale = mapScaleController.current();
 
@@ -39,43 +50,31 @@ function ControllerMessageBox(render) {
     }
     function onMapScaleChange() {
         for (const markerUid of renderedMessagesIds()) {
+            // todo обновление положения при изменении масштаба должно вычисляться быстрее, чем первичное размещение
+            //  и эффективнее если сразу для всего массива (возможно с сообщением об ошибке "недостаточно места")
             const newPos = acquireMessagePosition(markerUid);
             const messageContainer = getContainerOrNull(markerUid);
-            messageContainer.style.left = newPos[0] + 'px';
-            messageContainer.style.top = newPos[1] + 'px';
+            _setPosition(messageContainer, newPos)
             messLinksManager.update(markerUid, messageContainer);
         }
         // console.debug('map scale set to ', newScale);
     }
     function makeWrapper(position, size, mess) {
-        // const wrapper = document.createElementNS("http://www.w3.org/2000/svg",
-        //     'foreignObject');
         const wrapper = document.createElement('div');
         wrapper.style.position = 'absolute';
-        wrapper.style.left = position[0] + 'px';
-        wrapper.style.top = position[1] + 'px';
-        if (size !== undefined) {
-            wrapper.style.width = size[0] + 'px';
-            wrapper.style.height = size[1] + 'px';
-        }
+        _setPosition(wrapper, position)
+        _setSize(wrapper, size)
         wrapper.style.outline = 'none';
 
         wrapper.append(mess);
         wrapper.addEventListener('focus', handlerMessageDivFocus);
         return wrapper;
     }
-    function deduceContainer(layerTitle) {
-        const c = document.getElementsByClassName('layer_messages layer-' + layerTitle);
-        if (c.length > 0)
-            return c[0];
-        else
-            return undefined;
-    }
 
     function hideMessage(marker_uid) {
         const maybeMessItem = getContainerOrNull(marker_uid);
         if (maybeMessItem !== null) {
-            maybeMessItem.style.display = 'none';
+            _setDisplay(maybeMessItem, false);
             visibleMessagesIndex[marker_uid] = false;
             markerCirclesManager.setShown(marker_uid, false);
             return true;
@@ -88,7 +87,7 @@ function ControllerMessageBox(render) {
         // console.log('showMessage', markerPosition);
         const maybeMessItem = getContainerOrNull(marker_uid);
         if (maybeMessItem !== null) {
-            maybeMessItem.style.display = 'block';
+            _setDisplay(maybeMessItem);
             maybeMessItem.focus();
             visibleMessagesIndex[marker_uid] = true;
             markerCirclesManager.setShown(marker_uid, true);
@@ -101,19 +100,27 @@ function ControllerMessageBox(render) {
             return;
 
         // если сообщения ещё нет в индексе, нужно:
-        //   1) запросить место
-        //   2) если место нашлось запросить данные
-        //   3) если данные пришли, построить Node сообщения, закинуть в индекс и отобразить в контейнере
-        const position = acquireMessagePosition(marker_uid);
-        if (!position)      // todo нужны разные коды ошибок для разных ситуаций (какие ситуации описаны в тз?)
-            return;
-
+        //   1) запросить данные
+        //   2) если данные пришли, построить Node сообщения, закинуть в индекс и отобразить в контейнере
+        //   3) запросить и установить размещение. если места не нашлось, удалить
         doApiCall('GET', API_MARKER_GET_DATA(marker_uid), undefined,
             function (markerData) {
                 const messageElem = renderMessage(markerData);
-                const messContainer = makeWrapper(position, undefined, messageElem);
-                _registerMessageItem(markerData.marker, {messContainer: messContainer});
+                let messContainer = makeWrapper(undefined, undefined, messageElem);
+                const position = acquireMessagePosition(marker_uid);
+                if (!position) {
+                    // todo нужны разные коды ошибок для разных ситуаций (какие ситуации описаны в тз?)
+                    console.log('position=', position);
+                    messContainer = null;       // for proper garbage collection
+                    return;
+                }
+                else {
+                    _setPosition(messContainer, position)
+                    _setDisplay(messContainer)
+                }
+
                 container.append(messContainer);
+                _registerMessageItem(markerData.marker, {messContainer: messContainer});
                 messLinksManager.update(marker_uid, messContainer);
                 messContainer.focus();
             });
@@ -150,4 +157,30 @@ function ControllerMessageBox(render) {
         hideAll: hideAllMessages,
         onMapScaleChange: onMapScaleChange,
     }
+}
+
+function _setSize(wrapper, size) {
+    if (size !== undefined) {
+        wrapper.style.width = size[0] + 'px';
+        wrapper.style.height = size[1] + 'px';
+    }
+}
+
+function _setPosition(wrapper, position) {
+    if (position !== undefined) {
+        wrapper.style.left = position[0] + 'px';
+        wrapper.style.top = position[1] + 'px';
+    }
+    else {
+        wrapper.style.left = '0';
+        wrapper.style.top = '0';
+        wrapper.style.display = 'none';
+    }
+}
+
+function _setDisplay(wrapper, block=true) {
+    if (block)
+        wrapper.style.display = 'block';
+    else
+        wrapper.style.display = 'none';
 }
