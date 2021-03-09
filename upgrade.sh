a6fa3ab0251a959d3dd5e0693da888301da58396
Anton Iakovlev <lantar23@yandex.ru>
2021-03-09 12:30:06 +0300

    improve upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git pull;
+git pull || exit 1;
 docker-compose down;
 docker-compose up -d --build;
 docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' zoloto_viewer_postgres_1;
