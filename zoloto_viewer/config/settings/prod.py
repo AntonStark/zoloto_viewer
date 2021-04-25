c2e7a021f74ce234b077d28fa31e86331b548d0c
Anton Iakovlev <lantar23@yandex.ru>
2021-04-25 16:03:10 +0300

    fix boto3 headers

--- zoloto_viewer/config/settings/prod.py
+++ zoloto_viewer/config/settings/prod.py
@@ -42,8 +42,12 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
+AWS_EXPIREY = 1 * 24 * 60 * 60
+AWS_HEADERS = {
+    'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIREY, AWS_EXPIREY)
+}
 AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
+    'CacheControl': 'max-age=%d' % AWS_EXPIREY,
 }
 AWS_LOCATION = 'static'
 
